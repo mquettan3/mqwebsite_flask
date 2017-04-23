ef238f94eae082e563767b27fd2878fed14e1c04
Marcus Quettan <mquettan@Marcuss-MBP.hsd1.ga.comcast.net>
2017-04-23 10:26:35 -0400

    Removing *.pyc files from the repo.  Finally working wow animations.  Not entirely sure what was wrong but this works.  Now to figure out how to send an email then the site rework is done

--- mqwebsite/__init__.py
+++ mqwebsite/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
 from mqwebsite import views
--- mqwebsite/static/js/preloaderlogic.js
+++ mqwebsite/static/js/preloaderlogic.js
@@ -1,9 +1,35 @@
+
+
+//Scroll so that the header is taken into account.  Offset the scroll by the height of the header
+$('body').scrollspy({
+    target: '#header',
+    offset: 80
+})
+
+// Page slow-scrolling feature
+$('a.page-scroll').bind('click', function (event) {
+    var link = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(link.attr('href')).offset().top - 65
+    }, 500);
+    event.preventDefault();
+});
+
+
 //This file is for javascript that's local to the preloader.
 var loaded = false;
 
 //Preloader logic
 window.onload = function() {
     if(!loaded) {
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+            mobile: true,
+            live: true
+        });
+        wow.init();
         //Force the pre-loader to exist for at least 1 second.  Anything less is pretty jarring to see.
         setTimeout(function() { // Made the pre-loader not have a default wait time because Ryuga complained.
             $('.loader-wrapper').addClass('animated fadeOut');
